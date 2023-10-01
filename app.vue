2b7868c6b56c2f9d6c8ae73b5f2f630c54cfd53d
Andrey Dyldin <and@cesbo.com>
2023-10-01 10:16:36 +0300

    digitClick(), AC button

--- app.vue
+++ app.vue
@@ -1,19 +1,43 @@
 <template>
 <div class="p-10">
     <h1 class="text-xl mb-10">CalC</h1>
-    <div class="border border-gray-200 p-5">
+    <div
+        class="
+            border border-gray-200
+            p-5
+            space-y-5
+        "
+    >
         <div
             class="
                 border border-gray-200
-                mb-5 p-2
+                p-2
                 text-xl text-right
-
+                rounded
             "
-        >тут будут цифры</div>
+        >{{ line }}</div>
 
         <div
             class="
-                border border-gray-200 p-2
+                grid grid-cols-3 gap-2
+            "
+        >
+            <button
+                class="
+                    border border-gray-200
+                    text-xl
+                    py-2
+                    rounded
+                    shadow
+                    hover:bg-slate-50
+                    active:shadow-inner
+                "
+                @click="line = '0'"
+            >AC</button>
+        </div>
+
+        <div
+            class="
                 grid grid-cols-3 gap-2
             "
         >
@@ -26,9 +50,25 @@
                     rounded
                     shadow
                     hover:bg-slate-50
+                    active:shadow-inner
                 "
+                :class="{
+                    'col-start-2': i === 0,
+                }"
+                @click="digitClick(i)"
             >{{ i }}</button>
         </div>
     </div>
 </div>
 </template>
+
+<script setup lang="ts">
+const line = ref('0')
+
+function digitClick(digit: number) {
+    if(line.value == '0') {
+        line.value = ''
+    }
+    line.value = line.value + digit.toString()
+}
+</script>
